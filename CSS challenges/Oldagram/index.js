49f55c624a904610ed9a728287c6a2208352e62c
Sachini <sachichathudissanayaka@gmail.com>
2024-08-14 23:52:59 +0530

    Add changes

--- CSS challenges/Oldagram/index.js
+++ CSS challenges/Oldagram/index.js
@@ -3,27 +3,27 @@ import { posts } from "./posts.js";
 let mainEl = document.querySelector("main")
 
 function generatePosts(){
-    for(let i=0; i < posts.length; i++){
-        mainEl.innerHTML += `<section class="post">
+    posts.forEach(function(post){
+            mainEl.innerHTML += `<section class="post">
                     <div class="container">
                         <div class="user">
-                            <img class="sp" src="${posts[i].avatar}" alt="avatar image of ${posts[i].name}">
+                            <img class="sp" src="${post.avatar}" alt="avatar image of ${post.name}">
                             <div class="user-details">
-                                <p class="username size-12">${posts[i].name}</p>
-                                <p class="location">${posts[i].location}</p>
+                                <p class="username size-12">${post.name}</p>
+                                <p class="location">${post.location}</p>
                             </div>
                         </div>
-                        <img class="portrait" src="${posts[i].post}" alt="post of ${posts[i].name}">
+                        <img class="portrait" src="${post.post}" alt="post of ${post.name}">
                         <div class="icons">
                             <img src="images/icon-heart.png" alt="">
                             <img src="images/icon-comment.png" alt="">
                             <img src="images/icon-dm.png" alt="">
                         </div>
-                        <p class="likes size-12">${posts[i].likes} likes</p>
-                        <p class="caption size-12"><span class="name size-12">${posts[i].username}</span> ${posts[i].comment}</p>
+                        <p class="likes size-12">${post.likes} likes</p>
+                        <p class="caption size-12"><span class="name size-12">${post.username}</span> ${post.comment}</p>
                     </div>
                 </section>`
-    }
+    })
 
     const portraitImages = document.querySelectorAll(".portrait")
     portraitImages.forEach((image, index) => {
